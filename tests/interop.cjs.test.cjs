eaa03cf3327f11d2e5a7089dbd6daaef7e7ca9a5
Chris Osborn <c.osborn.contact@gmail.com>
2025-08-24 00:52:49 -0700

    chore: add changelog for interop test build

--- tests/interop.cjs.test.cjs
+++ tests/interop.cjs.test.cjs
@@ -1,7 +1,15 @@
-const cjs = require('../dist/index.cjs');
+const { execSync } = require('node:child_process');
 
-test('CJS build exports match ESM build', async () => {
-  const esm = await import('../dist/index.js');
+let cjs;
+let esm;
+
+beforeAll(async () => {
+  execSync('npm run build', { stdio: 'pipe' });
+  cjs = require('../dist/index.cjs');
+  esm = await import('../dist/index.js');
+});
+
+test('CJS build exports match ESM build', () => {
   const cjsKeys = Object.keys(cjs).sort();
   const esmKeys = Object.keys(esm).sort();
   expect(cjsKeys).toStrictEqual(esmKeys);
