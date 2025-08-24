25475295362918b44fc03d14773432ac0a68352b
cosbor11 <c.osborn.contact@gmail.com>
2025-08-24 11:26:46 -0700

    improved the documents examples

---
+++ examples/document/save-get-delete-document.ts
@@ -0,0 +1,41 @@
+// filename: examples/document/save-document.ts
+import process from 'node:process';
+import { Buffer } from 'node:buffer';
+import { onyx, type OnyxDocument } from '@onyx.dev/onyx-database';
+
+async function main(): Promise<void> {
+  const db = onyx.init();
+
+  const newDoc: OnyxDocument = {
+    documentId: 'note.json',
+    path: '/notes/note.json',
+    mimeType: 'application/json',
+    content: Buffer.from(JSON.stringify({ message: 'hello' })).toString('base64'),
+  };
+
+  const savedDoc = await db.saveDocument(newDoc);
+  console.log('Saved document:', savedDoc);
+
+   try {
+    const doc = await db.getDocument('note.json');
+    console.log('Document contents:', doc);
+  } catch {
+    console.log('Document not found.');
+  }
+
+    await db.deleteDocument('note.json');
+
+    try {
+    const doc = await db.getDocument('note.json');
+    console.log('oops document still exists:', doc);
+  } catch {
+    console.log('Document was deleted successfully');
+  }
+
+
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
