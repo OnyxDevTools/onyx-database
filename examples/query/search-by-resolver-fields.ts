990f8c5c1ac2db6548c07edfd529c4319079facb
cosbor11 <c.osborn.contact@gmail.com>
2025-09-05 15:59:48 -0700

    cleaned up the compound query response

---
+++ examples/query/search-by-resolver-fields.ts
@@ -0,0 +1,25 @@
+// filename: examples/query/search-by-resolver-fields.ts
+
+import process from 'node:process';
+import { eq, onyx } from '@onyx.dev/onyx-database';
+import { Schema, tables } from 'onyx/types';
+import { seed } from 'seed';
+
+async function main(): Promise<void> {
+  const db = onyx.init<Schema>({requestLoggingEnabled: true});
+
+  await seed();
+
+  const logs = await db
+    .from(tables.User)
+    .where(eq("roles.name", "Admin"))
+    .resolve('roles')
+    .list();
+
+  console.log(JSON.stringify(logs, null, 2));
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
